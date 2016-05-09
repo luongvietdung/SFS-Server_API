0b0f689c808eb4c67541117527ed7287202c1d18
Lương Việt Dũng <luong.viet.dung@framgia.com>
2016-05-09 14:30:27 +0700

    Change root page
    Edit root page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth',
     skip: [:omniauth_callbacks, :confirms, :registers, :recovers, :token_validations],
     controllers: {sessions: 'sessions'}
-  root "api/users#index"
+  root "api/locations#index"
   namespace :api do
     resources :users, only: [:update, :show] do
       resources :locations, only: [:create, :update]
