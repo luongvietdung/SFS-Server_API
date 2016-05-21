61274aa8083bf9ff1a75943f4fac7d8c8953f16c
DungLV <luong.viet.dung@framgia.com>
2016-05-22 00:54:12 +0700

    Add date for serializer

--- app/serializers/detail_order_serializer.rb
+++ app/serializers/detail_order_serializer.rb
@@ -1,3 +1,7 @@
 class DetailOrderSerializer < ApplicationSerializer
-  attributes :id, :phone_shop, :phone_customer, :phone_shipper, :code, :code_checking, :status
+  attributes :id, :phone_shop, :phone_customer, :phone_shipper, :code, :code_checking, :status, :date
+
+  def date
+    created_at.to_date
+  end
 end
